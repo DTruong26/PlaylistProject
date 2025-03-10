956fed3bdf41975ce9d9fa56d2ade563a6df5b42
Russell Thai <rthai27@students.polytechic.org>
2025-03-09 20:45:40 -0700

    fixing errors for submission

--- Playlist.java
+++ Playlist.java
@@ -9,8 +9,8 @@ public class Playlist {
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
-
-
+    
+     ArrayList<Song> mySongs;
 
 
      /**
@@ -18,7 +18,87 @@ public class Playlist {
       * then use additional methods to add Songs in one-by-one
       */
 
+      public Playlist()
+      {
+      mySongs = new ArrayList<Song>();
+      }
+      
+      public void addSong(String name, String artist, String dur, boolean liked)
+      {
+        Song track = new Song(name, artist, dur, liked);
+        mySongs.add(track);
+      }
 
+      public ArrayList<Song> likedSongs()
+      {
+        ArrayList<Song> goodSongs = new ArrayList<Song>();
+        for(int i = 0; i < mySongs.size(); i++)
+        {
+            if(mySongs.get(i).isLiked())
+            {
+                goodSongs.add(mySongs.get(i));
+            }
+        }
+        return goodSongs;
+      }
+
+      public void like(String name)
+      {
+        for(int i = 0; i < mySongs.size(); i++)
+        {
+            if(mySongs.get(i).isLiked() == false && mySongs.get(i).getName().equals(name))
+            {
+                mySongs.get(i).like();
+            }
+        }
+      } 
+
+      public void removeSong(String name)
+      {
+        for(int i = mySongs.size() - 1; i >= 0; i--) 
+        {
+            if(mySongs.get(i).getName().equals(name))
+            {
+                mySongs.remove(i);
+            }
+        }
+      }
+
+    
+
+      public double totalDuration()
+      {
+        double sum = 0;
+        for(int i = 0; i < mySongs.size(); i++)
+        {
+            sum += mySongs.get(i).calcDuration();
+        }
+        return (sum / 60);
+      }
+      
+      
+
+      public void removeUnliked()
+      {
+        for(int i = 0; i < mySongs.size(); i++)
+        {
+          if(mySongs.get(i).isLiked() == false)
+          {
+            mySongs.remove(i);
+          }
+        }
+      }
+
+      public String allSongs()
+      {
+        String list = "";
+        for(int i = 0; i < mySongs.size(); i++)
+        {
+          list += mySongs.get(i).getName() + ", ";
+        }
+        return list;
+      }
+      
       
 
       /**
--- Song.java
+++ Song.java
@@ -4,32 +4,67 @@
  * @version
  */
 public class Song {
-    //Fields-- what information do we want each Song to store?
-    private String name;
-    private String author;
-    private int time;
+  //Fields-- what information do we want each Song to store?
+  private String songName;
+  private String songDur;
+  private boolean liked;
+  private String artist;
+  
+
+  /**
+   * Constructor-- what information needs to be given to make a Song?
+   * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+   */
+  public Song(String songName, String artist, String songDur, boolean liked)
+  {
+      this.songName = songName;
+      this.songDur = songDur;
+      this.liked = false;
+      this.artist = artist;
+  }
 
 
 
-    /**
-     * Constructor-- what information needs to be given to make a Song?
-     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
-     */
 
-
-
-
-
-     /**
-      * Methods-- what will you want each Song to do?
-      * Consider all the getter methods (getName, getArtist, etc.)
-      * You should probably have a toString method to be able to get the information for the full
-      * song easily as well!
-      * What kind of mutator (setter) methods will you need?
-      */
-
-      public int getTime(){
-        return time;
-      }
-
+   /**
+    * Methods-- what will you want each Song to do?
+    * Consider all the getter methods (getName, getArtist, etc.)
+    * You should probably have a toString method to be able to get the information for the full
+    * song easily as well!
+    * What kind of mutator (setter) methods will you need?
+    */
+    public String getName()
+    {
+          return songName;  
+    }
+    public String getArtist()
+    {
+        return artist;
+    }
+    public String songDur()
+    {
+        return songDur;
+    }
+    public boolean isLiked()
+    {
+        return liked;
+    }
+    public void like()
+    {
+        liked = true;
+    }
+    public void unlike()
+    {
+        liked = false;
+    }
+    public double calcDuration()
+    {
+        double sec = 0;
+        String min = songDur.substring(0, songDur.indexOf(":"));
+        sec += Integer.parseInt(min) * 60;
+        String secPart = songDur.substring(songDur.indexOf(":") + 1, songDur.length());
+        //String secPart = songDur.substring(0, songDur.indexOf(":") + 1);
+        sec += Integer.parseInt(secPart);
+        return sec;
+    }
 }
